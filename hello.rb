af5d896ec05e341f006e4e2e3360cd0cfafae13e
Kevin Allsworth <kevin.allsworth@siteminder.com.au>
2011-03-21 12:57:01 +1100

    added xml parser and hotel hash

--- hello.rb
+++ hello.rb
@@ -1,3 +1,18 @@
-print('Enter your name: ' )
+require 'rubygems'
+require 'xmlsimple'
+
+
+hotelHashList = Hash.new
+
+print('Enter a hotel name: ' )
 name = gets()
 puts( "Hello #{name}" )
+print('Enter description for this hotel:')
+desc = gets()
+hotelHashList[name] = desc
+
+print(hotelHashList)
+
+hotelsXml = XmlSimple.xml_out(hotelHashList, 'RootName' => "Hotel")
+
+print(hotelsXml)
