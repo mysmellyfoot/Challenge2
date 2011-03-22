f57092a411571db0bbe7f67115b9d22a7864232b
Kevin Allsworth <KevinAllsworth@Kevin-Allsworths-iMac.local>
2011-03-22 22:41:47 +1100

    wip

--- hello.rb
+++ hello.rb
@@ -2,17 +2,63 @@ require 'rubygems'
 require 'xmlsimple'
 
 
-hotelHashList = Hash.new
+class Hotel
+  attr_accessor :name, :description
+ 
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+  
+  def to_s
+      @name + ", " + @description
+    end
+end
 
-print('Enter a hotel name: ' )
-name = gets()
-puts( "Hello #{name}" )
-print('Enter description for this hotel:')
-desc = gets()
-hotelHashList[name] = desc
+def saySomething()
+  puts "\e[H\e[2J"
+   print("Welcome to my extraordinary Hotels DB\n")
+   print("Please select an option: Q to Quit\n")
+   print("\n")
+   print("1. View all Hotels\n")
+   print("2. Add Hotel\n")
+   print("3. Change Hotel\n")
+   print("4. Delete Hotel\n")
+   print("5. Search\n")
+   print("Q. Quit\n")
+end
 
-print(hotelHashList)
 
-hotelsXml = XmlSimple.xml_out(hotelHashList, 'RootName' => "Hotel")
+hotelHashList = {}
 
-print(hotelsXml)
+saySomething
+
+
+cmd = ""
+until cmd == 'Q' do
+   print ("What is your bidding my master? ")
+     cmd = gets
+    print cmd
+    
+    print cmd == 'Q'
+    end
+
+hotelsXml = XmlSimple.xml_out(hotelHashList, 'RootName' => "HotelList")
+
+
+
+def printHotelList() 
+  print(HotelHashList.to_s)
+end  
+
+
+
+def addHotel()
+  puts( "Hello #{name}" )
+  print('Enter description for this hotel:')
+  desc = gets()
+  hotelHashList[name] = Hotel.new(name,desc)
+end
+
+
+
